461c3b4017f01d88e8d71d56ffc6887759af28be
Arsh Sharma <88606045+arsharmaa@users.noreply.github.com>
2023-01-19 14:21:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySelectorAll('[data-skip]');
-const ranges = player.querySelectorAll('.player__slider');
+// const skipButtons = player.querySelectorAll('[data-skip]');
+// const ranges = player.querySelectorAll('.player__slider');
